7b9e01184c047d96dc41512afc4046c8c3716c50
tohir <st.coder2005@gmail.com>
2024-01-11 15:52:33 +0500

    Leetcode Solutions in Python

--- PythonSolutions/1-two_sum.py
+++ PythonSolutions/1-two_sum.py
@@ -1,7 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numsHash = {}
-        for i in range(len(nums)):
-            if target - nums[i] in numsHash:
-                return [numsHash[target - nums[i]], i]
-            numsHash[nums[i]] = i
+def twoSum(nums: list[int], target: int) -> list[int]:
+    numsHash = {}
+    for i in range(len(nums)):
+        if target - nums[i] in numsHash:
+            return [numsHash[target - nums[i]], i]
+        numsHash[nums[i]] = i
+            
+nums1 = [2,7,11,15]
+target1 = 9
+
+nums2 = [3,2,4]
+target2 = 6
+
+nums3 = [3,3]
+target3 = 6
+
+twosum = twoSum(nums2,target2)
+print(twosum)
