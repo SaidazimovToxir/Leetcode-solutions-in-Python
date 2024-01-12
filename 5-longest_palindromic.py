17a751c9524108baab7d5468cc8b5f0ad31a34bf
tohir <st.coder2005@gmail.com>
2024-01-12 23:30:27 +0500

    Leetcode Solutions in Python

---
+++ 5-longest_palindromic.py
@@ -0,0 +1,36 @@
+""" 
+5. Longest Palindromic Substring
+"""
+
+
+def isPalindrome(s,start,end):
+    while start<end:
+        if s[start] != s[end]:
+            return False
+        start +=1
+        end -=1
+
+    return True
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        start = 0
+        m = 1
+        for i in range(len(s)):
+            for j in range(i+1,len(s)):
+                if isPalindrome(s,i,j) and j-i+1 > m:
+                    start = i
+                    m = j-i+1
+        
+        return s[start:start + m]
+      
+      
+def main():
+    solution = Solution()
+    
+    s = "babad"
+    result = solution.longestPalindrome(s)
+    print(result)
+
+if __name__=='__main__':
+    main()    
---
+++ PythonSolutions/4-median_two.py
@@ -0,0 +1,28 @@
+""" 
+4. Median of Two Sorted Arrays
+"""
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        nums1+=nums2
+        nums1.sort()
+
+        lenght=len(nums1)
+        if lenght%2==0:
+            a = lenght//2
+            return (nums1[a-1] + nums1[a]) / 2.0
+        else:
+            a = lenght//2
+            return float(nums1[a])
+        
+def main():
+    solution = Solution()
+    
+    # Testcase
+    nums1 = [1,3] 
+    nums2 = [2]
+    result = solution.findMedianSortedArrays(nums1,nums2)
+    print(result)
+    
+if __name__=='__main__':
+    main()
